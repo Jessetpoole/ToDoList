938def00900bd7c352ea64c3bf5401f84e4353c0
cgreenup <charlirejunk@gmail.com>
2020-05-06 20:12:45 -0500

    TodoTask class basic functionality created

--- app/src/main/java/edu/csci/todolist/TodoTask.java
+++ app/src/main/java/edu/csci/todolist/TodoTask.java
@@ -5,17 +5,32 @@ import android.util.Log;
 import java.util.Calendar;
 
 public class TodoTask {
-    //TODO: get Current date
-    //TODO: get time of task
 
     //TODO: store in database
     //TODO: Create class for checking database for next TODO with time
     //TODO: check every 1 or 5 minutes, depending on how I set it up
 
-    int
 
-    TodoTask(int hour, int minutes){
-        Log.i("TESTTT", "" + Calendar.getInstance().getTime());
+    //Variables to store when the task is supposed to occur
+    private int hour;
+    private int minute;
+    private int date;
+
+    TodoTask(int iHour, int iMinutes){
+        hour = iHour;
+        minute = iMinutes;
+        date = Calendar.DAY_OF_MONTH;
     }
+
+    TodoTask(){
+        hour = 23;
+        minute = 59;
+        date = Calendar.DAY_OF_MONTH;
+    }
+
+
+    public int getHour()    { return hour; }
+    public int getMinute()  { return minute; }
+    public int getDate()    { return date; }
 
 }
